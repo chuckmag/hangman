a92bbb58c8480a258467d0def4b2dfc1045e0209
chuckmag <ccmagrane@gmail.com>
2017-04-09 15:44:48 -0400

    Added a snackbar to start game after dismissing dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Optional } from '@angular/core';
 import { HangmanGameState } from './hangmanGameState';
 import { HangmanService } from './hangmanService/hangman.service';
 
-import { MdDialog, MdDialogRef } from '@angular/material';
+import { MdDialog, MdDialogRef, MdSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -14,23 +14,40 @@ import { MdDialog, MdDialogRef } from '@angular/material';
 export class AppComponent implements OnInit {
   title = 'Hang Man!';
   hangmanGameState: HangmanGameState;
+  snackBarShown: boolean = false;
 
-  constructor(private hangmanService: HangmanService, private _dialog: MdDialog) {
+  constructor(private hangmanService: HangmanService,
+    private _dialog: MdDialog,
+    private _snackbar: MdSnackBar) {
     this.hangmanGameState = new HangmanGameState();
   }
 
   ngOnInit(): void {
     this.getHangmanGameState();
   }
+  
+  showSnackbar(message: string) {
+    let snackBarRef = this._snackbar.open(message, 'Start New Game!');
+    this.snackBarShown = true;
+    snackBarRef.onAction().subscribe(() => {
+      this.snackBarShown = false;
+      this.startNewHangmanGame();
+    });
+  }
 
   setHangmanGameState(hangmanGameState: HangmanGameState): void {
     this.hangmanGameState = hangmanGameState;
     console.log(hangmanGameState);
 
+    if (this.snackBarShown) {
+      return;
+    }
+
     if (hangmanGameState.gameOver && hangmanGameState.incorrectGuessCount < 10) {
       let dialogRef = this._dialog.open(WinContent);
       dialogRef.afterClosed().subscribe(startNewGame => {
         if (!startNewGame) {
+          this.showSnackbar("WOW SUCH WORD SKILLZ!");
           return;
         }
         this.startNewHangmanGame();
@@ -39,6 +56,7 @@ export class AppComponent implements OnInit {
       let dialogRef = this._dialog.open(LoseContent);
       dialogRef.afterClosed().subscribe(startNewGame => {
         if (!startNewGame) {
+          this.showSnackbar("Try again?");
           return;
         }
         this.startNewHangmanGame();
@@ -86,7 +104,7 @@ export class WinContent {
   template: `
     <h1>OH NO! You lost.</h1>
     <p>
-      Click the button below to start a new game.
+      Click the button below to start a new game, or click of this dialog to continue guessing.
     </p>
     <p> <button md-raised-button color="primary" (click)="dialogRef.close(true)">Start new Game.</button> </p>
   `,
