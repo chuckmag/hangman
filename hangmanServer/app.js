a350a829e1e0e77574d7514f03e1fde18518c634
chuckmag <ccmagrane@gmail.com>
2017-04-10 22:43:17 -0400

    ng build puts to express path and npm start serves the app root.

--- hangmanServer/app.js
+++ hangmanServer/app.js
@@ -8,7 +8,29 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+var db = require('./db/hangmanDb');
+
 var app = express();
+
+var cache = require('memory-cache');
+ 
+// now just use the cache 
+ 
+cache.put('foo', 'bar');
+console.log(cache.get('foo'));
+ 
+// that wasn't too interesting, here's the good part 
+ 
+cache.put('houdini', 'disappear', 100, function(key, value) {
+    console.log(key + ' did ' + value);
+}); // Time in ms 
+ 
+console.log('Houdini will now ' + cache.get('houdini'));
+ 
+setTimeout(function() {
+    console.log('Houdini is ' + cache.get('houdini'));
+}, 200);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +46,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/', express.static('bin/angular'))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
