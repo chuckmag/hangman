a08f159a27d5cb84f0e6b7c7b7fae140173a2e16
chuckmag <ccmagrane@gmail.com>
2017-04-13 17:45:14 -0400

    Starting to add some more api methods

--- hangmanServer/db/hangmanGameState.js
+++ hangmanServer/db/hangmanGameState.js
@@ -11,15 +11,56 @@ var hangmanSchema = mongoose.Schema({
 
 var HangmanGameState = mongoose.model('HangmanGameState', hangmanSchema);
 
+exports.HangmanGameState = HangmanGameState;
 
 exports.findAll = function () {
     return HangmanGameState.find().exec();
 }
-
-exports.HangmanGameState = HangmanGameState;
 
 // returns the promise
 // .then(function (hangman) {console.log(hangman)});
 exports.findById = function (req) {
   return HangmanGameState.findById(req.params.id).exec();
 };
+
+
+// returns the promise
+// .then(function (hangman) {console.log(hangman)});
+exports.findById = function (req) {
+  return HangmanGameState.findById(req.params.id).exec();
+};
+
+
+// returns the promise
+// .then(function (hangman) {console.log(hangman)});
+exports.updateById = function (req) {
+
+    var hangman = req.body;
+
+
+
+    return HangmanGameState.findById(req.params.id).exec();
+};
+
+exports.getNewGameState = function() {
+
+    var secretWord = 'thing';
+
+    var hangmanGameState = new HangmanGameState({hiddenWord: "____",
+        guesses: '',
+        incorrectGuesses: '',
+        incorrectGuessCount: 0,
+        gameOver: false,
+        secretWord: 'thing'
+    });
+
+    hangmanGameState.save(function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(hangmanGameState);
+        }
+    });
+
+    return new Promise(hangmanGameState);
+}
--- hangmanServer/routes/api.js
+++ hangmanServer/routes/api.js
@@ -23,16 +23,45 @@ db.once('open', function callback() {
 
   router.get('/:resource', function (req, res, next) {
     res.type('application/json');
-    repositories[req.params.resource].findAll().then(function (hangmans){
-      console.log(hangmans);
+    repositories[req.params.resource].findAll().then(function (resources){
+      console.log(resources);
+      res.json(resources);
     });
   });
 
   router.get('/:resource/:id', function (req, res, next) {
     res.type('application/json');
-    //repositories[req.params.resource].findOne(req, res);
+    repositories[req.params.resource].findById(req).then(function (resource) {
+        console.log(resource);
+        res.json(hangman);
+    });
   });
 
+  router.put('/:resource/:id', function (req, res, next) {
+    res.type('application/json');
+    repositories[req.params.resource].updateById(req).then(function (hangman) {
+        console.log(hangman);
+        res.json(hangman);
+    });
+  });
+
+  // this is only for testing purposes
+  router.post('/:resource/', function (req, res, next) {
+    res.type('application/json');
+
+    req.body.HangmanGameState;
+    repositories[req.params.resource].create(req).then(function (hangman) {
+        console.log(hangman);
+        res.json(hangman);
+    });
+  });
+
+  router.get('/:resource/getNewGameState/', function (req, res, next) {
+      repositories[req.params.resource].getNewGameState().then(function (hangmang) {
+          console.log(hangman);
+          res.json(hangman);
+      });
+  });
 });
 
 module.exports = router;
